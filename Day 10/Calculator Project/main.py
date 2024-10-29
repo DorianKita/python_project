4487acc1b5ca4632beecbff1220c5e28507775df
Dorian Kita <dorian.kita90@gmail.com>
2024-10-29 11:06:52 +0100

    calculator using recursion principals

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,3 +1,5 @@
+from art import logo
+
 def add(n1, n2):
     return n1 + n2
 
@@ -10,3 +12,34 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(logo)
+
+def calculator():
+    should_acc = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_acc:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the second number?: "))
+        result = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+
+        choice = input(f"Type 'y' to continue calculatet with {result}, or 'n' to start a new calculations")
+
+        if choice == "y":
+            num1 = result
+        else:
+            should_acc = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
