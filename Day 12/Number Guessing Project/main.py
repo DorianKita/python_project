e049edf81f57f4945460edf2e695d2b3f0dd8c50
Dorian Kita <dorian.kita90@gmail.com>
2024-11-01 11:23:16 +0100

    guess a number by myself

---
+++ Day 12/Number Guessing Project/main.py
@@ -0,0 +1,45 @@
+import random
+from art import logo
+
+def generate_number():
+    return random.randint(1,100)
+
+def check_if_correct(number,users_guess):
+    if number == users_guess:
+        return True
+
+def choose_difficulty(choice):
+    if choice == 'easy':
+        return 10
+    elif choice == 'hard':
+        return 5
+
+def game():
+    random_number = generate_number()
+    users_guess = -1
+    attempts = 0
+
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    attempts = choose_difficulty(difficulty)
+
+    for i in range(attempts):
+        print(f"You have {attempts} attempts to guess the correct number.")
+        users_guess = int(input("Make a guess: "))
+        if check_if_correct(random_number,users_guess):
+            print(f"Congratulations! You've found correct number {random_number}")
+            return
+        else:
+            if users_guess > random_number:
+                print("Too high.")
+                attempts -= 1
+            else:
+                print("Too low.")
+                attempts -= 1
+
+    print(f"You lost. Correct answer was {random_number}")
+
+game()
